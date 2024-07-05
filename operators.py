ea7723133e29db4c1179ec3668ca7e85bb29a0ea
Hasnain Khan <134506777+hasnainkhan2004@users.noreply.github.com>
2024-07-05 13:22:18 +0500

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,24 @@
+print (2+2)
+print (45-4)
+print (3*8)
+print (35/5)
+
+a=True
+b=True
+if a and b:
+    print ("All True!")
+
+a=True
+b=False
+if a or b:
+    print ("All True!")
+
+a=False
+b=False
+if not(a) or not(b):
+    print ("All True!")
+
+a=True
+b=False
+if not(a) or not(b):
+    print ("All True!")
