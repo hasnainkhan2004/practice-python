bbcd3b2072af2733c106e2161beee2a4dff322cb
Hasnain Khan <134506777+hasnainkhan2004@users.noreply.github.com>
2024-07-07 14:40:22 +0500

    Add files via upload

---
+++ exception.py
@@ -0,0 +1,14 @@
+def divide_by(a, b):
+    return a / b
+
+try:
+    ans = divide_by(40, 0)
+except:
+    print('Something went wrong!')
+
+try:
+    ans = divide_by(40, 0)
+except Exception as e:
+    print('Something went wrong!', e)
+
+    print(e.__class__)
