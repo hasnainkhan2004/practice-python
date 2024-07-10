dd80cfbdeba58ab69857b92da8c0c8f7e27ca321
Hasnain Khan <134506777+hasnainkhan2004@users.noreply.github.com>
2024-07-10 11:09:50 +0500

    Add files via upload

---
+++ Namespacing_and_scope.py
@@ -0,0 +1,15 @@
+# SCOPES: built-in, global, local
+def d():
+    animal = "elephant"
+    def e():
+        nonlocal animal
+        animal = "giraffe"
+        print("Inside nested function: " + animal)
+        
+    print("Before calling function: " + animal)
+    e()
+    print("After nested function: " + animal)
+
+animal = "camel"
+d()
+print("Global animal: " + animal)
